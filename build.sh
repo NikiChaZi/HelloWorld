ea1998e0b094610db68bb735da5ddf094aafaafb
NikiChaZi <snitckii.np@dvfu.ru>
2025-09-25 18:48:15 +1000

    commit

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e
-
-echo "🔄 Updating project from Git..."
-git pull origin main || echo "⚠️ Не удалось обновить, продолжаем со старыми файлами"
-
-echo "🛠️ Building project..."
-mkdir -p build
+git pull origin main || true
+rm -rf build
+mkdir build
 cd build
 cmake ..
 make
-
-echo "✅ Build complete. Run program with: ./HelloWorld"
 
